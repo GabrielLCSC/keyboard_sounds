4b71cef45ea2de1828b8d1b2fa0392ac0e099d01
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-07-20 14:17:54 +0200

    🎉 push the project on git

---
+++ main.py
@@ -0,0 +1,36 @@
+from tkinter import *
+fen = Tk()
+
+import pygame
+pygame.init()
+son_touche_a = pygame.mixer.Sound("C:/laragon/www/py-keyboard/crash.wav")
+son_touche_z = pygame.mixer.Sound("C:/laragon/www/py-keyboard/miaou.wav")
+
+
+def press(event):
+    print(event.keysym)
+    if event.keysym == 'a':
+        son_touche_a.play()
+    if event.keysym == 'Return':
+        son_touche_z.play()
+        
+# def release(event):
+#     print(event.keysym)
+# test
+fen.bind_all('<KeyPress>', press)
+# fen.bind_all('<KeyRelease>', release)
+fen.mainloop()
+
+
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.KEYDOWN:
+#             print("Touche enfoncée : ", event.key)
+#             if event.key == pygame.K_a:
+#                 print("Touche 'a' détectée")
+#                 son_touche_a.play()
+#                 pygame.time.delay(100)
+#             elif event.key == pygame.K_z:
+#                 print("Touche 'z' détectée")
+#                 son_touche_b.play()
+#                 pygame.time.delay(100)
